958915cb601ba58151bd9d5c37a8a1c5fdd8b376
gsrhead <s2k9krpm00@gmail.com>
2023-02-19 18:45:20 -0500

    DadJokes

--- react-joke-box/src/App.js
+++ react-joke-box/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const API_URL = `https://icanhazdadjoke.com`;
 
@@ -6,17 +7,18 @@ function App() {
   const [joke, setJoke] = useState("");
 
   const generateJoke = () => {
-    fetch(`${API_URL}/search`, {
+    axios.get(`${API_URL}/search`, {
       headers: {
         Accept: "application/json",
+        "User-Agent": "randomJokeGenerator GitHub@gsrhead",
       },
     })
-      .then((res) => res.json())
-      .then((data) => {
-        const randomIndex = Math.floor(Math.random() * data.results.length);
-        const randomJoke = data.results[randomIndex];
+      .then((res) => {
+        const randomIndex = Math.floor(Math.random() * res.data.results.length);
+        const randomJoke = res.data.results[randomIndex];
         setJoke(randomJoke.joke);
-      });
+      })
+      .catch((err) => console.log(err));
   };
 
   useEffect(() => {
