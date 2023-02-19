9799606e88e9aa948c92f2153bb27bdf79fc1229
gsrhead <s2k9krpm00@gmail.com>
2023-02-19 18:35:18 -0500

    dadJokes

--- react-joke-box/src/App.js
+++ react-joke-box/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+const API_URL = `https://icanhazdadjoke.com`;
 
 function App() {
+  const [joke, setJoke] = useState("");
+
+  const generateJoke = () => {
+    fetch(`${API_URL}/search`, {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const randomIndex = Math.floor(Math.random() * data.results.length);
+        const randomJoke = data.results[randomIndex];
+        setJoke(randomJoke.joke);
+      });
+  };
+
+  useEffect(() => {
+    generateJoke();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <h2>DAD RANDOMS</h2>
+      <p>{joke}</p>
+      <button onClick={generateJoke}>Get Dad Jokes</button>
     </div>
   );
 }
